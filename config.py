0d96b45e0e434263e5a142746914bfba4c2e0378
Lukasavicus <lukasavicus@gmail.com>
2020-06-10 15:14:30 -0300

    Add pre-README and License, update requirements and config

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 DEBUG = True # Turns on debugging features in Flask
+SECRET_KEY = "safra-dev-app"
+
 ALLOWED_EXTENSIONS = {'zip', 'py'}
 SUCCESS = {'status' : "OK"}
 ERROR = {'status' : "ERROR"}
+
 PIPENV_IGNORE_VIRTUALENVS=1
+
 MONGO_URI =  "mongodb://127.0.0.1:27017/wm" # windmill
-URL_BASE = "apl-wm-crm"
+
+APPLICATION_ROOT = "/apl-wm-crm"
+
+LOG_FLAG = False
+LOGGER_NAME = "wm-logger"
 
 #BCRYPT_LOG_ROUNDS = 12 # Configuration for the Flask-Bcrypt extension
 #MAIL_FROM_EMAIL = "lukasavicus@gmail.com" # For use in application emails
